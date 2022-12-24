059154a17da6418202f455d4252e712ef7551b4d
shashank adhikari <shashank@shashanks-MacBook-Air.local>
2022-12-24 11:31:25 +0545

    final with babel, webpack-dev-server and gsap for animation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
+import fun from "./assets/fun.svg";
 
-console.log(generateJoke());
+const laughImg = document.getElementById("laughImg");
+
+laughImg.src = fun;
+
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener("click", () => {
+  generateJoke();
+  tl.fromTo(".container", { scale: 0 }, { scale: 1 });
+  tl.fromTo(".joke", { opacity: 0, y: -50 }, { opacity: 1, y: 0 });
+  tl.fromTo(".btn", { opacity: 0, y: -50 }, { opacity: 1, y: 0 });
+});
+generateJoke();
+
+// animation starts
+
+const tl = gsap.timeline({ defaults: { duration: 0.75 } });
+
+tl.fromTo(".container", { scale: 0 }, { scale: 1 });
+tl.fromTo(".joke", { opacity: 0, y: -50 }, { opacity: 1, y: 0 });
+tl.fromTo(".btn", { opacity: 0, y: -50 }, { opacity: 1, y: 0 });
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
   },
+  devtool: "source-map",
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
@@ -25,6 +28,20 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
